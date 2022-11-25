f5e5aa9f5ece43742cc7989c58007db550793d10
Kristiyan Belyaev <kristiyan.belyaev@systemhaus-ulm.de>
2022-11-25 15:23:40 +0100

    download taste bei Fotos hinzugefügt

--- resources/views/upload/create.blade.php
+++ resources/views/upload/create.blade.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
 <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    <div>
-        <a href="/guest" class="text-white absolute m-4">Zurück</a>
+<body class="bg-violet-500 h-full">
+    <div class="flex items-center text-white">
+        <a href="/guest" class="flex m-4 text-lg"><span class="flex font-bold">⇦</span>Zurück</a>
     </div>
-    <section class="px-6 py-6 bg-violet-500 h-screen flex items-center justify-center">
+    <section class="flex items-center justify-center">
         <main class=" m-8 px-4 py-4 w-1/4 border b rounded-xl bg-gray-200">
             <h1 class="text-4xl flex justify-center">Senden Sie Ihre Foto!</h1>
                 <form method="POST" action="/upload" class="mt-10" enctype="multipart/form-data">
@@ -83,18 +83,19 @@
                             <p class="color: red; font-size: small">{{ $message }}</p>
                         @enderror
                     </div>
-                    <div class="mb-6">
-                        <label class="mb-2 uppercase font-bold text-xs"
+                    <div class=" flex">
+                        <label class=" uppercase font-bold text-xs flex"
                             for="extra"
                         >
-                        Wollen Sie es schnellst möglich haben?
+                        Wollen Sie es schnellst möglich haben?<br>
                         </label>
-                        <input class=" ml-24 w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        <input class=" flex cursor-pointer ml-24 w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                             type="checkbox"
                             name="extra"
                             id="extra"
                         >
                     </div>
+                        <div class="mb-4 text-gray-500">(extra 5€)</div>
                     <div class="mb-6">
                         <button type="submit"
                                 class="bg-yellow-400 rounded py-2 px-4 hover:bg-yellow-500"
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -10,11 +10,14 @@
     </div>
     <main class="text-lg font-bold mt-12 px-4 py-4">
         <h2 class="m-4">Name: <?= $user->name ?></h2>
-        <div class="m-4">Email:
-        <h2 class="" id="email"> <?= $user->email ?></h2>
-            <button onclick="copyToClipboard('#email')" class="text-sm flex text-gray-500">Copy email</button>
+        <div class="m-4 flex">Email:
+        <h2 class="mx-4" id="email"> <?= $user->email ?></h2>
+            <button onclick="copyToClipboard('#email')" class="text-sm text-gray-500">Copy email</button>
         </div>
-        <h2 class="m-4">Photo: <img src="/storage/{{ $user->photo }}" alt="Photo" class="max-w-lg"></h2>
+        <div class="flex items-end">
+            <h2 class="m-4">Photo: <img src="/storage/{{ $user->photo }}" alt="Photo" class="max-w-lg"></h2>
+            <a href="/storage/{{ $user->photo }}" download="{{ $user->photo }}" target="_blank" class="et_pb_button m-4 p-4 py-3 bg-orange-500 text-white rounded-xl">Download</a>
+        </div>
         <h2 class="m-4">Task: <?= $user->task ?></h2>
     </main>
     <script>
